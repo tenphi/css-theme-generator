04106540a03bbeb2d59d06faf3c27b1011f0d43a
Andrey Yamanov <tenphi@gmail.com>
2024-04-01 16:25:42 +0200

    chore: use subdomain for deployment * 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/css-theme-generator/',
   plugins: [vue({
     template: {
       compilerOptions: {
