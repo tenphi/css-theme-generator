0a1b9dd10e8aab32af4dae0fd7756734ca03f3d1
Andrey Yamanov <tenphi@gmail.com>
2024-04-01 10:03:40 +0200

    chore: auto-publishing * 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/css-theme-generator/',
   plugins: [vue({
     template: {
       compilerOptions: {
